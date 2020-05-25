7e4a1175d21c89a293869aa49e53701eff460293
Manuhmutua <manuh.mutuah@gmail.com>
2020-05-25 22:45:57 +0300

    added aws configure cli

--- cmd/apply_file.go
+++ cmd/apply_file.go
@@ -13,11 +13,8 @@ import (
 var applyCmd = &cobra.Command{
 	Use:   "apply",
 	Short: "Apply AWS config files",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long: `Use this command to set the aws configurations before running the list command. For example:
+cli-tool-golang apply -f <Absolute Path of the configuration file>.`,
 	Run: func(cmd *cobra.Command, args []string) {
 
 		fstatus, _ := cmd.Flags().GetBool("file")
